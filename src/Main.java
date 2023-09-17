8fa3e1cbd91885a93231e81bc9c1befe72d49a9c
Surya <pandiarajsurya@gmail.com>
2023-09-17 21:34:40 +0530

    files added

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        RWOnly obj=new RWOnly();
-//        obj.name = "Surya";         //private variable cannot be accessed in another class
-        obj.setName("Surya");
-        System.out.print(obj.getName());
-    }
-}
