eb59f9025efee9e8bd031ff2cd587fab57fb49a1
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2023-09-17 19:40:19 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+//        obj.name = "Surya";         //private variable cannot be accessed in another class
+        obj.setName("Surya");
+        System.out.print(obj.getName());
+    }
+}
---
+++ src/RWOnly.java
@@ -0,0 +1,15 @@
+class RWOnly{
+    private String name;
+
+    public RWOnly() {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
