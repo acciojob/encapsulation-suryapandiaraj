e59c9d7c438fa6336f7aec56510fd85b250a398c
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2023-09-17 19:47:45 +0530

    Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class Main {
-  
+      RWOnly obj=new RWOnly();
+//        obj.name = "Surya";         //private variable cannot be accessed in another class
+        obj.setName("Surya");
+        System.out.print(obj.getName());
 }
