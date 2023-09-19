338b5ce3e982f9cbcd2c01d0db9969e2497d40ee
Surya <pandiarajsurya@gmail.com>
2023-09-19 13:11:12 +0530

    new changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,10 @@
 package com.driver;
 
 public class Main {
-      RWOnly obj=new RWOnly();
-//        obj.name = "Surya";         //private variable cannot be accessed in another class
+    public static void main(String[] args){
+        RWOnly obj=new RWOnly();
+//        obj.name="Surya";   //private variable cannot be accessed in another class
         obj.setName("Surya");
         System.out.print(obj.getName());
+    }
 }
